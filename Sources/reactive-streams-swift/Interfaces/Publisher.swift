f65447388dc2c90573dc52af85b997a5008800e9
Ben Pinhorn <00ben.allen@gmail.com>
2018-09-02 16:55:20 -0400

    Added in-code documentation to all interfaces and their functions. Stopped tests from failing.

--- Sources/reactive-streams-swift/Interfaces/Publisher.swift
+++ Sources/reactive-streams-swift/Interfaces/Publisher.swift
@@ -1,14 +1,42 @@
-//
-//  Publisher.swift
-//  reactive-streams-swift
-//
-//  Created by Ben Pinhorn on 2018-09-01.
-//
+/************************************************************************
+ * Licensed under Public Domain (CC0)                                    *
+ *                                                                       *
+ * To the extent possible under law, the person who associated CC0 with  *
+ * this code has waived all copyright and related or neighboring         *
+ * rights to this code.                                                  *
+ *                                                                       *
+ * You should have received a copy of the CC0 legalcode along with this  *
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.*
+ ************************************************************************/
+
+/**
+ Publisher.swift
+ reactive-streams-swift
+ 
+Created by Ben Pinhorn on 2018-09-01.
+ 
+ A Publisher is a provider of a potentially unbounded number of sequenced elements, publishing them according to
+ the demand received from its Subscriber(s).
+ 
+ A Publisher can serve multiple Subscribers subscribed dynamically
+ at various points in time.
+ 
+ 
+ */
 
 import Foundation
 
 public protocol Publisher {
     
+    /**
+    
+    Subscribe a Subscriber to this Publisher, to receive elements from it
+ 
+    Request elements using the Subscription given back by the Publisher using onSubscribe(), by calling Subscription.request(num of elements)
+     
+     - parameter subscriber: The subscriber which will receive the elements given by the Publisher
+     
+    */
     func subscribe(subscriber: Subscriber)
     
 }
--- Tests/reactive-streams-swiftTests/reactive_streams_swiftTests.swift
+++ Tests/reactive-streams-swiftTests/reactive_streams_swiftTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import reactive_streams_swift_framework
+@testable import reactive_streams_swift
 
 final class reactive_streams_swift_frameworkTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(reactive_streams_swift_framework().text, "Hello, World!")
     }
 
 
